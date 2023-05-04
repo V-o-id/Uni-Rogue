aa05373d8b99a9efe173164b7c52e1ed90309f8b
moritz_haider <moritz.haider@sz-ybbs.ac.at>
2023-05-04 11:35:33 +0200

    Choose Player character impemented

--- core/src/com/mygdx/game/states/OptionState.java
+++ core/src/com/mygdx/game/states/OptionState.java
@@ -4,18 +4,19 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.assets.loaders.AssetLoader;
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import com.mygdx.game.sprites.Font;
 import com.mygdx.game.sprites.Text;
 import com.badlogic.gdx.utils.ScreenUtils;
 
@@ -28,7 +29,9 @@ public class OptionState extends State {
     private Stage stage;
     Label character;
 
-    OptionState(GameStateManager gsm){
+    private TextButton textButton;
+
+    OptionState(final GameStateManager gsm){
         super(gsm);
         ExtendViewport extendViewPort = new ExtendViewport(700, 1200, new OrthographicCamera());
         stage = new Stage(extendViewPort);
@@ -40,14 +43,29 @@ public class OptionState extends State {
         table.defaults().pad(10);
         table.setFillParent(true);
 
-        character = new Label("Character: ", skin);
+        character = new Label("Enter Character: ", skin);
         inputField = new TextField("", skin);
+        textButton = new TextButton("Enter", skin);
 
         table.add(character);
         table.add(inputField).width(300);
+        table.add(textButton);
 
         stage.addActor(table);
+
+        textButton.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                System.out.println("clicked");
+                gsm.set(new MenuState(gsm));
+                //write character into textfile
+                FileHandle file = Gdx.files.local("selectedCharacter.txt");
+                file.writeString(inputField.getText(), false);
+            }
+        });
+
         Gdx.input.setInputProcessor(stage);
+
         /* inputField = new TextField("", );
         inputField.setPosition(0, 0);
         inputField.setMessageText("Enter character");
